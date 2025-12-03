ba4a3bff46620813e38f118a0bf7ac0890e26a58
Mejiiaa <luisenriquecruz78@gmail.com>
2025-12-02 20:34:06 -0600

    correxion
    web

--- routes/web.php
+++ routes/web.php
@@ -106,7 +106,6 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {    // Gestión
     });
 });
 
->>>>>>> main
 // Ruta para activar un semestre (solo un semestre activo a la vez)
 Route::post('admin/semestres/{id}/activar', [SemestresCursadosController::class, 'activarSemestre'])
     ->middleware('auth')
