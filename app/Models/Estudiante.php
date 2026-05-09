93be93f438223d20bc8835d673bb48aa449f277a
Mejiiaa <luisenriquecruz78@gmail.com>
2026-05-09 14:13:30 -0600

    Columna genero
    se realizo la parte del migrate de la columna genero

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -18,17 +18,24 @@ class Estudiante extends Model
         'nombre',
         'numero_control',
         'carrera',
+        'sexo', // <-- Columna agregada 
         'semestre',
         'id_actividad',
         'id_unidad',
         'id_semestre'
     ];
 
+    /**
+     * Relación con el modelo Actividad
+     */
     public function actividad()
     {
         return $this->belongsTo(\App\Models\Actividad::class, 'id_actividad', 'id_actividad');
     }
 
+    /**
+     * Relación con el modelo Unidad
+     */
     public function unidad()
     {
         return $this->belongsTo(\App\Models\Unidad::class, 'id_unidad', 'id_unidad');
